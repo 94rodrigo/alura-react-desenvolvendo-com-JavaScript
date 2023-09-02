c5d2d9dc4adc116461d263347a8750234d9b4bde
Rodrigo Costa <rodrigoc94@hotmail.com>
2023-09-02 19:33:50 -0300

    Correções

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,7 +9,7 @@ const Time = (props) => {
         props.colaboradores.length > 0 && <section className='time' style={estilosCss}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} key={colaborador.nome}/>)}
+                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} key={colaborador.nome}/>)}
             </div>
         </section>
     )
